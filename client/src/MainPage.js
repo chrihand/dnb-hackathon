5127cf8340e7b0e204158521bc0a03d951f10e3a
oscarcarlstrom <oscar.carlstrom@gmail.com>
2017-09-17 10:12:43 +0200

    First page
    Added map and button at bottom

--- client/src/MainPage.js
+++ client/src/MainPage.js
@@ -19,65 +19,70 @@ class MainPage extends Component {
           </div>
         </header>
         <div className="container">
-          <h1 className="green-text">
-            It's a match!
-          </h1>
-          <h2 className="green-text lovers-top">
-            You
-          </h2>
-          <span className="green-text heart">♡</span>
-          <h2 className="green-text lovers-bottom">
-            [Area]
-          </h2>
+	    	<div className="card">
+	          <h1 className="green-text">
+	            It's a match!
+	          </h1>
+	          <h2 className="green-text lovers-top">
+	            You
+	          </h2>
+	          <span className="green-text heart">♡</span>
+	          <h2 className="green-text lovers-bottom">
+	            [Area]
+	          </h2>
+	        </div>
+	        <div className="circles">
+	          <div className="circle-wrapper green-text">
+	            <div className="circle">
+	              <div className="value">
+	                40
+	              </div>
+	              <div className="unit">
+	                m<span className="raised">2</span>
+	              </div>
+	            </div>
+	            <h3>
+	              Expected floor space
+	            </h3>
+	          </div>
+	          <div className="circle-wrapper green-text">
+	            <div className="circle">
+	              <div className="value">
+	                15
+	              </div>
+	              <div className="unit">
+	                min
+	              </div>
+	            </div>
+	            <h3 className="green-text">
+	              To city centre
+	            </h3>
+	          </div>
+	          <div className="circle-wrapper green-text">
+	            <div className="circle">
+	              <div className="value">
+	                27
+	              </div>
+	              <div className="unit">
+	                years
+	              </div>
+	            </div>
+	            <h3 className="green-text">
+	              Average age
+	            </h3>
+	          </div>
+	        </div>
 
-          <div className="circle-wrapper green-text">
-            <div className="circle">
-              <div className="value">
-                40
-              </div>
-              <div className="unit">
-                m<span className="raised">2</span>
-              </div>
-            </div>
-            <h3>
-              Expected floor space
-            </h3>
-          </div>
-          <div className="circle-wrapper green-text">
-            <div className="circle">
-              <div className="value">
-                15
-              </div>
-              <div className="unit">
-                min
-              </div>
-            </div>
-            <h3 className="green-text">
-              To city centre
-            </h3>
-          </div>
-          <div className="circle-wrapper green-text">
-            <div className="circle">
-              <div className="value">
-                27
-              </div>
-              <div className="unit">
-                years
-              </div>
-            </div>
-            <h3 className="green-text">
-              Average
-            </h3>
-          </div>
-
-          <h1 className="green-text">What's up at [Area]?</h1>
-          <Instagram/>
-          {/*<User/>*/}
+         	<h1 className="green-text">What's up at [Area]?</h1>
+        	<Instagram/>
+        	{/*<User/>*/}
+        	<button>Find me another area</button>
         </div>
 
-        <div className="map-background">
 
-        </div>
+
+        <div className="map-background"></div>
+        <div className="scroll-indicator"></div>
       </div>
     );
   }
